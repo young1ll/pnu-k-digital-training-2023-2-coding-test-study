6175c83cd14cc63df818b176499241d1e5909d9d
Young1ll <goooglecho01@gmail.com>
2023-04-04 17:00:35 +0900

    Week01 그리디알고리즘 5문제 완료

---
+++ youngill/그리디알고리즘/BAEKJOON_11399.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BAEKJOON_11399 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        int time = 0;
+        int result = 0;
+
+        for(int i=0; i < n; i++) arr.add(sc.nextInt());
+        Collections.sort(arr);
+
+        for(int i=0; i < arr.size(); i++) {
+            time += arr.get(i);
+            result += time;
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
